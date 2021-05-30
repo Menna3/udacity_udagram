1b61d138255ac395a0f9acc15b555b81d2bada70
Menna Atef <menna.atef@silkbytes.com>
2021-05-30 12:01:07 +0200

    added Typescript types

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
-  app.get('/filteredimage', async ( req, res ) => {
+  app.get('/filteredimage', async (req:express.Request, res:express.Response) => {
     let url = req.query.image_url;
     if (url) {
         filterImageFromURL(url).then((response)=> {
